4bf90040b8fc24a1d1be4dd4772ce00b7074148a
TimmBmn <TimmBmn@gmail.com>
2024-02-27 12:22:30 +0100

    Git Blame added to lualine + removed lazy-lock.json

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,30 @@
 return {
 	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	dependencies = {
+		'nvim-tree/nvim-web-devicons',
+		"f-person/git-blame.nvim"
+	},
 
-	-- calls "require("lualine").setup()
-	config = true
+	config = function()
+
+		vim.g.gitblame_display_virtual_text = 0
+		vim.g.gitblame_date_format="%r"
+		vim.g.gitblame_message_template = '<author> (<date>)'
+		local git_blame = require("gitblame")
+
+
+		local function gitblameShowCondition()
+			return git_blame.is_blame_text_available() and git_blame.get_current_blame_text() ~= nil
+		end
+
+		require("lualine").setup({
+			sections = {
+				lualine_c = {
+					{ git_blame.get_current_blame_text, cond = gitblameShowCondition}
+				}
+			}
+		})
+
+
+	end
 }

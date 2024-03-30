2c376e7709a29681b24ddb87699bcc710c517a09
TimmBmn <TimmBmn@gmail.com>
2024-03-30 22:27:14 +0100

    added leaderpp for explorer

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,2 +1,2 @@
 vim.keymap.set("n", "<leader>e", ":lua vim.diagnostic.open_float(0, {scope=\"line\"})<CR>")
-vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>pp", vim.cmd.Ex)

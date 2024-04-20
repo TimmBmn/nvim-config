93f955e14d40795af3d5e37dd06a5de51947adbc
TimmBmn <TimmBmn@gmail.com>
2024-04-20 13:27:01 +0200

    Add keybind for undotree back
    removed it on accident lol

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>e", ":lua vim.diagnostic.open_float(0, {scope=\"line\"})<CR>")
+vim.keymap.set("n", "<leader>ut", ":UndotreeToggle<CR>")
 vim.keymap.set("n", "<leader>pp", vim.cmd.Ex)

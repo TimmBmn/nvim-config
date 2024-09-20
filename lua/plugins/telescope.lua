1f1d342b3df2bee3bc7cdf2436426db970bacce7
Tim Baumann <TimmBmn@gmail.com>
2024-09-20 20:17:15 +0200

    fixed telescope not installing the very first time

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,11 @@
 return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {},
     keys = {
-        { "<leader>ff", require("telescope.builtin").find_files, mode="n" },
-        { "<leader>fg", require("telescope.builtin").live_grep, mode="n" },
-        { "<leader>fb", require("telescope.builtin").buffers, mode="n" },
-        { "<leader>gs", require("telescope.builtin").git_status, mode="n" }
+        { "<leader>ff", "<cmd>Telescope find_files<CR>", mode="n" },
+        { "<leader>fg", "<cmd>Telescope live_grep<CR>", mode="n" },
+        { "<leader>fb", "<cmd>Telescope buffers<CR>", mode="n" },
+        { "<leader>gs", "<cmd>Telescope git_status<CR>", mode="n" }
     }
 }

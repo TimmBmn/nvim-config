39cdb459202d8b911d5f27cd17f549a351089dff
Tim Baumann <TimmBmn@gmail.com>
2024-09-20 20:12:15 +0200

    added autocompletion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,46 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        { "williamboman/mason.nvim", opts = {} },
-        { "williamboman/mason-lspconfig.nvim", opts = {} },
-        { "hrsh7th/cmp-nvim-lsp" },
-        { "hrsh7th/nvim-cmp" },
-    },
-    event = { "BufReadPre", "BufNewFile" },
-    config = function ()
+    { "williamboman/mason.nvim",           opts = {} },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            { "williamboman/mason-lspconfig.nvim", opts = {} },
+            {
+                "hrsh7th/nvim-cmp",
+                config = function()
+                    local cmp = require("cmp")
+                    cmp.setup({
+                        sources = {
+                            { name = "nvim_lsp" }
+                        },
+                        mapping = cmp.mapping.preset.insert({
+                            -- these are the only two i am actively using right now
+                            -- in the future i'll probably add the scrolling through docs
+                            -- but for now its enough
+                            ['<C-Space>'] = cmp.mapping.complete(),
+                            ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                            --['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                            --['<C-f>'] = cmp.mapping.scroll_docs(4),
+                            --['<C-e>'] = cmp.mapping.abort(),
+                        }),
+                    })
+                end,
+            },
+            { "hrsh7th/cmp-nvim-lsp" },
+        },
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local lspconfig = require("lspconfig")
+            local mason_lspconfig = require("mason-lspconfig")
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-        local lspconfig = require("lspconfig")
-        local mason_lspconfig = require("mason-lspconfig")
-        local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-        mason_lspconfig.setup_handlers({
-            -- default handler for installed servers
-            function(server_name)
-                lspconfig[server_name].setup({
-                    capabilities = cmp_nvim_lsp.default_capabilities()
-                })
-            end,
-        })
-    end
+            mason_lspconfig.setup_handlers({
+                -- default handler for installed servers
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = cmp_nvim_lsp.default_capabilities()
+                    })
+                end,
+            })
+        end,
+    }
 }

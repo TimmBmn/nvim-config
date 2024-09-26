7322c8392ebedc0e50ba281bc191ddb7c8335645
Tim Baumann <TimmBmn@gmail.com>
2024-09-26 21:18:44 +0200

    refactor

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,8 +3,7 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     opts = {
         on_attach = function(bufnr)
-            local opts = {}
-            opts.buffer = bufnr
+            local opts = { buffer = bufnr}
 
             local gitsigns = require("gitsigns")
             vim.keymap.set("n", "<leader>gbl", gitsigns.blame_line, opts)
